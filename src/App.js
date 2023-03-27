54a02b5c3c66b00b86dd0237f424cd2b635a89a0
ArtemTuba <tuba9521@gmail.com>
2023-03-27 10:58:50 +0300

    Add home work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState} from "react";
 function App() {
+
+  const [smileys, setSmileys] = useState([
+    { emoji: "😀", count: 0 },
+    { emoji: "😍", count: 0 },
+    { emoji: "😂", count: 0 },
+    { emoji: "🤔", count: 0 },
+  ]);
+
+  const handleClick = (index) => {
+    const newSmileys = [...smileys];
+    newSmileys[index].count++;
+    setSmileys(newSmileys);
+  };
+
+  const [winner, setWinner] = useState(null);
+
+  const showResults = () => {
+    let maxCount = -1;
+    let winningSmiley = null;
+    smileys.forEach((smiley) => {
+      if (smiley.count > maxCount) {
+        maxCount = smiley.count;
+        winningSmiley = smiley.emoji;
+      }
+    });
+    setWinner(winningSmiley);
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {smileys.map((smiley, index) => (
+        <div key={smiley.emoji}>
+          <span>{smiley.emoji}</span>
+          <span>{smiley.count}</span>
+          <button onClick={() => handleClick(index)}>Like</button>
+        </div>
+      ))}
+      <button onClick={showResults}>Show Results</button>
+      <div>The winner is: {winner}</div>
     </div>
   );
 }
